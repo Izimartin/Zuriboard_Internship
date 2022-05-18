921b5444e49a9ed49682bd260ac6561cea00ea89
Izimartin <izimartinz@gmail.com>
2022-05-18 03:08:25 +0100

    Simple CLI Calculator

---
+++ Python/Calculator.py
@@ -0,0 +1,29 @@
+# A Python calculator that can perform: addition, subtraction, division, multiplication and modulus operations. It should accept user input
+
+# Store input numbers:
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+# Subtract two numbers
+min = float(num1) - float(num2)
+# Multiply two numbers
+mul = float(num1) * float(num2)
+# Divide two numbers
+div = float(num1) / float(num2)
+# Modulo two numbers
+mod = float(num1) % float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Display the subtraction
+print('The subtraction of {0} and {1} is {2}'.format(num1, num2, min))
+# Display the multiplication
+print('The multiplication of {0} and {1} is {2}'.format(num1, num2, mul))
+# Display the division
+print('The division of {0} and {1} is {2}'.format(num1, num2, div))
+# Display the modulus
+print('The modulus of {0} and {1} is {2}'.format(num1, num2, mod))
