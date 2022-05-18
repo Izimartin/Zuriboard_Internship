94b661f02b31085ffd3be31e2eb2fb447c744bc8
Izimartin <izimartinz@gmail.com>
2022-05-18 03:08:05 +0100

    Adding Integers
    Write a simple python program that adds 2 numbers and print output

---
+++ Python/Adding_Integers.py
@@ -0,0 +1,8 @@
+# A simple python program that adds 2 numbers together and output the result my your terminal.
+
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+sum = int(x) + int(y)
+
+print("The sum is: ", sum)
